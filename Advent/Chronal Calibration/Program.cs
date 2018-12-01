fc56a3122c0ff23ebfbf19461d37a7192a19dd1d
ixtermanator <jameslearn1@gmail.com>
2018-12-01 17:39:52 -0500

    Finished part 2 answer is 83130

--- Advent/Chronal Calibration/Program.cs
+++ Advent/Chronal Calibration/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Chronal_Calibration
@@ -19,16 +20,33 @@ namespace Chronal_Calibration
             string line = string.Empty;
             StreamReader reader = new StreamReader(input);
 
-            while ((line = reader.ReadLine()) != null)
+            int? dup = null;
+            List<int> dupCheck = new List<int>();
+            dupCheck.Add(frequency);
+
+            while ((line = reader.ReadLine()) != null || dup == null)
             {
+                if (line == null)
+                {
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                }
+
                 int delta = 0;
                 if (Int32.TryParse(line, out delta))
                 {
                     frequency += delta;
+                    if (dupCheck.Contains(frequency))
+                    {
+                        dup = frequency;
+                        break;
+                    }
+                    dupCheck.Add(frequency);
                 }
             }
 
             Console.WriteLine("End frequency: {0}", frequency);
+            Console.WriteLine("First duplicate frequency: {0}", dup);
             Console.ReadKey();
         }
     }
